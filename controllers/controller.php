d41ed3370e6ff494fcd226b93c65ee3c9cce335e
Tien Han <tienthuyhan@gmail.com>
2024-05-29 06:58:27 -0400

    Add UPS font to application, update styling for homepage

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,21 +1,36 @@
 <?php
+
+/**
+ * The controller class defines methods that can be called for routing
+ * within the UPS Business Leads project.
+ *
+ * @author Tien Han <tienthuyhan@gmail.com>
+ * @date   5/29/2024
+ */
+class Controller
+{
+    //Instance variables
+    private $_f3; //Fat-Free Router
+
     /**
-     * Controller class for the UPS Business Leads project
-     * 328/business-leads/controllers/controller.php
+     * Constructor creates a controller object, which instantiates Fat-Free
+     *
+     * @param Base $f3 Fat-Free
      */
-    class Controller
+    function __construct($f3)
     {
-        private $_f3; //Fat-Free Router
-
-        function __construct($f3)
-        {
-            $this->_f3 = $f3;
-        }
-
-        function homepage(): void
-        {
-            //Render a view page
-            $view = new Template();
-            echo $view->render('views/homepage.html');
-        }
+        $this->_f3 = $f3;
     }
+
+    /**
+     * The method for rendering the homepage
+     *
+     * @return void
+     */
+    function homepage(): void
+    {
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/homepage.html');
+    }
+}
