2b71e7cb3e25b0531a508eef915b3cd2fcd9def5
Tien Han <tienthuyhan@gmail.com>
2024-06-01 07:53:08 -0400

    Move the rest of the routes over to Controller and rename formSummary.html to form-summary.html for standardization

--- controllers/controller.php
+++ controllers/controller.php
@@ -5,7 +5,7 @@
  * within the UPS Business Leads project.
  *
  * @author Tien Han <tienthuyhan@gmail.com>
- * @date   5/29/2024
+ * @date   6/01/2024
  */
 class Controller
 {
@@ -119,6 +119,77 @@ class Controller
         session_destroy();
     }
 
+    /**
+     * The method for rendering the FAQ page
+     *
+     * @return void
+     */
+    function faq(): void
+    {
+        $view = new Template();
+        echo $view->render('views/faq.html');
+    }
+
+    /**
+     * The method for rendering the login page
+     *
+     * @return void
+     */
+    function login(): void
+    {
+        //If the user has submitted a post request (filled out the login form)
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //Get submitted form data
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $allValid = true;
+
+            //Perform validation on the submitted username (email)
+            $emailError = '';
+            if (!empty($email)) {
+                if (!validateUPSEmail($email)) {
+                    $allValid = false;
+                    $emailError = 'Please enter in a valid UPS email!';
+                }
+            } else {
+                //Email is required
+                $allValid = false;
+                $emailError = 'Please enter an email.';
+            }
+            $this->_f3->set('SESSION.email', $email);
+            $this->_f3->set('SESSION.emailError', $emailError);
+
+            //Perform validation on the submitted password
+            $passwordError = '';
+            if (!empty($password)) {
+                if (!validatePassword($password)) {
+                    $allValid = false;
+                    $passwordError = 'Please enter in a valid password!';
+                }
+            } else {
+                //Password is required
+                $allValid = false;
+                $passwordError = 'Please enter your password.';
+            }
+            $this->_f3->set('SESSION.passwordError', $passwordError);
+
+            //Redirect to the application dashboard
+            if ($allValid) {
+                $this->_f3->reroute("dashboard");
+            }
+        }
+
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/login.html');
+    }
+
+    /**
+     * The method for rendering the reset password request email page
+     *
+     * @return void
+     */
     function passwordReset(): void
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -215,6 +286,7 @@ class Controller
         echo $view->render('views/password-request.html');
     }
 
+    //TODO: Add in PHP Doc block about what this method renders
     function passwordEmail()
     {
         // if the correct items are in the link used to reach this page AND
@@ -255,25 +327,283 @@ class Controller
                 // check if
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     // if the page has posted, set the password
-                        $password = $_POST["password"];
-                        echo $password;
-                        // check if it's a valid password
-                        if (validatePassword($password)) {
-                            $sql = "UPDATE users SET password = :password WHERE email = :email";
-                            $statement = $dbh->prepare($sql);
-                            $statement->bindParam(":password", $password);
-                            $statement->bindParam(":email", $email);
-                            $statement->execute();
-                        } else {
-                            $this->_f3->set('errors["reset"]', "please enter a valid password");
-                        }
+                    $password = $_POST["password"];
+                    echo $password;
+                    // check if it's a valid password
+                    if (validatePassword($password)) {
+                        $sql = "UPDATE users SET password = :password WHERE email = :email";
+                        $statement = $dbh->prepare($sql);
+                        $statement->bindParam(":password", $password);
+                        $statement->bindParam(":email", $email);
+                        $statement->execute();
+                    } else {
+                        $this->_f3->set('errors["reset"]', "please enter a valid password");
                     }
-                } else {
-                    // if we get here, their code was expired
-                    die("This code is expired");
                 }
+            } else {
+                // if we get here, their code was expired
+                die("This code is expired");
             }
-            $view = new Template();
-            echo $view->render('views/password-reset.html');
         }
+        $view = new Template();
+        echo $view->render('views/password-reset.html');
+    }
+
+    /**
+     * The method for rendering the signup page
+     *
+     * @return void
+     */
+    function signUp(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            $slic = $_POST['slic'];
+            $role = $_POST['role'];
+            $password = $_POST['password'];
+
+            //construct new user object
+            $this->_f3->set('SESSION.user', new User());
+
+            //validate first name
+            if (validateName($firstName)) {
+                $this->_f3->get('SESSION.user')->setFirstName($firstName);
+            } else {
+                $this->_f3->set('errors["firstNameSignUpError"]', "please enter a valid first name");
+            }
+
+            //validate last name
+            if (validateName($lastName)) {
+                $this->_f3->get('SESSION.user')->setLastName($lastName);
+            } else {
+                $this->_f3->set('errors["lastNameSignUpError"]', "please enter a valid last name");
+            }
+
+            //validate UPS email
+            if (validateUPSEmail($email)) {
+                $this->_f3->get('SESSION.user')->setEmail($email);
+            } else {
+                $this->_f3->set('errors["emailSignUpError"]', "please enter a valid UPS email");
+            }
+
+            //validate slic
+            if (validSlic($slic)) {
+                $this->_f3->get('SESSION.user')->setSlic($slic);
+            } else {
+                $this->_f3->set('errors["slicSignUpError"]', "please enter a valid SLIC");
+            }
+
+            //role
+            if (validRole($role)) {
+                $this->_f3->get('SESSION.user')->setRole($role);
+            } else {
+                $this->_f3->set('errors["roleSignUpError"]', "please enter a valid role");
+            }
+
+            //password
+            if (validatePassword($password)) {
+                $this->_f3->get('SESSION.user')->setPassword($password);
+            } else {
+                $this->_f3->set('errors["passwordSignUpError"]', "please enter a valid password");
+            }
+
+            //check errors[]
+            if (empty($this->_f3->get('errors'))) {
+                //send email to DM to approve sign up request and add to DB
+                $status = 0;
+                $date = date('Y-m-d h:i:s', time());
+                $subject = "Access Requested From: " . $this->_f3->get('SESSION.user')->getFirstName()." ". $this->_f3->get('SESSION.user')->getLastName();
+                $to = "garrett.ballreich101@gmail.com";
+                //msg should be a link to approve the request
+                require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
+
+                try {
+                    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+                    echo 'connected to database!';
+                } catch (PDOException $e) {
+                    die($e->getMessage());
+                }
+
+                //First
+                $sql = 'INSERT INTO users (first_name, last_name, email, password, account_activated, role, created_at) 
+                    VALUES(:First, :Last, :Email, :Password, :AccountActivated, :Role, :CreatedAt)';
+
+                $statement = $dbh->prepare($sql);
+                $statement->bindParam(':First', $firstName);
+                $statement->bindParam(':Last', $lastName);
+                $statement->bindParam(':Email', $email);
+                $statement->bindParam(':Password', $password);
+                $statement->bindParam(':AccountActivated', $status);
+                $statement->bindParam(':Role', $role);
+                $statement->bindParam(':CreatedAt', $date);
+                $statement->execute();
+
+                $msg = "https://garrettballreich.greenriverdev.com/328/business-leads/approval";
+                mail($to, $subject, $msg);
+
+                $this->_f3->reroute("dashboard");
+            }
+        }
+
+        $view = new Template();
+        echo $view->render('views/sign-up.html');
+    }
+
+    /**
+     * The method for rendering the approval page.
+     * All new users to approval will be listed on this page.
+     *
+     * @return void
+     */
+    function approval(): void
+    {
+        //connect to db
+        require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
+
+        try {
+            $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            echo 'connected to database!';
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+        $sql = "SELECT * FROM users WHERE account_activated = 0";
+
+        //prepare the statement
+        $statement = $dbh->prepare($sql);
+
+        //execute the statement
+        $statement->execute();
+
+        //process
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        echo "<h1>Approve Requests</h1>";
+        echo "<ol>";
+        foreach($result as $row){
+            echo "<li>".$row['last_name']. ", ".$row['first_name']."-".$row['']."</li>";
+        }
+        echo "</ol>";
+        $view = new Template();
+        echo $view->render('views/approval.html');
+    }
+
+    /**
+     * Method for rendering the main form, where business leads are submitted
+     *
+     * @return void
+     */
+    function mainForm(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $businessName = $_POST['businessName'];
+            $businessAddress = $_POST['businessAddress'];
+            $contactName = $_POST['firstName'];
+            $contactLastName = $_POST['lastName'];
+            $businessPhone = $_POST['phoneNum'];
+            $contactEmail = $_POST['email'];
+            $driverName = $_POST['driverName'];
+            $driverID = $_POST['driverID'];
+            $slic = $_POST['slic'];
+
+            //construct new lead object
+            //add it to the session
+            //with all unknown values until they pass validation
+            $this->_f3->set("SESSION.lead", new Lead());
+            //set each variable to an object field
+
+            //validate name
+            //all letter and not null
+            if (validateName($businessName)) {
+                $this->_f3->get('SESSION.lead')->setBusinessName($businessName);
+            } else {
+                $this->_f3->set('errors["businessNameError"]', "please enter a valid name");
+            }
+
+            //validate contact name
+            if (validateName($contactName)) {
+                $this->_f3->get('SESSION.lead')->setContactName($contactName);
+            } else {
+                $this->_f3->set('errors["contactNameError"]', "please enter a valid contact name");
+            }
+
+            //validate contact lastname
+            if (validateName($contactLastName)) {
+                $this->_f3->get('SESSION.lead')->setContactLastName($contactLastName);
+            } else {
+                $this->_f3->set('errors["contactLastNameError"]', "please enter a valid contact last name");
+            }
+
+            //validate phone number
+            if (validPhone($businessPhone)) {
+                $this->_f3->get('SESSION.lead')->setBusinessPhone($businessPhone);
+            } else {
+                $this->_f3->set('errors["businessPhoneError"]', "please enter a valid phone number");
+            }
+
+            //validate email
+            if (validateEmail($contactEmail)) {
+                $this->_f3->get('SESSION.lead')->setContactEmail($contactEmail);
+            } else {
+                $this->_f3->set('errors["contactEmailError"]', "please enter a valid email address");
+            }
+
+            //validate driver name
+            if (validateName($driverName)) {
+                $this->_f3->get('SESSION.lead')->setDriverName($driverName);
+            } else {
+                $this->_f3->set('errors["driverNameError"]', "please enter a valid contact last name");
+            }
+
+            //validate employee ID
+            if (validEmployeeID($driverID)) {
+                $this->_f3->get('SESSION.lead')->setDriverID($driverID);
+            } else {
+                $this->_f3->set('errors["driverIDError"]', "please enter a valid employee ID");
+            }
+
+            //validate slic
+            if (validSlic($slic)) {
+                $this->_f3->get('SESSION.lead')->setSlic($slic);
+            } else {
+                $this->_f3->set('errors["slicError"]', "please enter a slic in the North West Division");
+            }
+
+            //check errors[]
+            if (empty($this->_f3->get('errors'))) {
+                //add lead object to DB
+                //email lead object to perspective center manager
+                $this->_f3->reroute('form-summary');
+            }
+        }
+
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/form.html');
+    }
+
+    /**
+     * Method for rendering the main form's summary page
+     *
+     * @return void
+     */
+    function formSummary(): void
+    {
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/form-summary.html');
+    }
+
+    /**
+     * Method for rendering the dashboard page
+     *
+     * @return void
+     */
+    function dashboard(): void
+    {
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/dashboard.html');
+    }
 }
--- index.php
+++ index.php
@@ -36,308 +36,67 @@ $f3-> route('GET /', function() {
 /**
  * Route to the "Contact Us" page
  */
-$f3-> route('GET|POST /contact', function($f3) {
+$f3-> route('GET|POST /contact', function() {
     $GLOBALS['controller']->contactUs();
 });
 
-//Define the FAQ route
+/**
+ * Route to the "FAQ" page
+ */
 $f3-> route('GET /faq', function() {
-    //Render a view page
-    $view = new Template();
-    echo $view->render('views/faq.html');
+    $GLOBALS['controller']->faq();
 });
 
-//Define the Login route
-$f3-> route('GET|POST /login', function($f3) {
-    //If the user has submitted a post request (filled out the login form)
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //Get submitted form data
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $allValid = true;
-
-        //Perform validation on the submitted username (email)
-        $emailError = '';
-        if (!empty($email)) {
-            if (!validateUPSEmail($email)) {
-                $allValid = false;
-                $emailError = 'Please enter in a valid UPS email!';
-            }
-        } else {
-            //Email is required
-            $allValid = false;
-            $emailError = 'Please enter an email.';
-        }
-        $f3->set('SESSION.email', $email);
-        $f3->set('SESSION.emailError', $emailError);
-
-        //Perform validation on the submitted password
-        $passwordError = '';
-        if (!empty($password)) {
-            if (!validatePassword($password)) {
-                $allValid = false;
-                $passwordError = 'Please enter in a valid password!';
-            }
-        } else {
-            //Password is required
-            $allValid = false;
-            $passwordError = 'Please enter your password.';
-        }
-        $f3->set('SESSION.passwordError', $passwordError);
-
-        //Redirect to the application dashboard
-        if ($allValid) {
-            $f3->reroute("dashboard");
-        }
-    }
-
-    //Render a view page
-    $view = new Template();
-    echo $view->render('views/login.html');
+/**
+ * Route to the "Login" page
+ */
+$f3-> route('GET|POST /login', function() {
+    $GLOBALS['controller']->login();
 });
 
-// send user to the page to request a reset password email
+/**
+ * Route to the reset password request email page
+ */
 $f3->route('GET|POST /password-request', function() {
     $GLOBALS['controller']->passwordReset();
 });
 
-// loads the emailed password reset page
+/**
+ * Route to the emailed password reset page
+ */
 $f3->route('GET|POST /password-email', function() {
     $GLOBALS['controller']->passwordEmail();
 });
 
-// define the signup route
-$f3-> route('GET|POST /sign-up', function($f3) {
-    //var_dump($_POST);
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $firstName = $_POST['firstName'];
-        $lastName = $_POST['lastName'];
-        $email = $_POST['email'];
-        $slic = $_POST['slic'];
-        $role = $_POST['role'];
-        $password = $_POST['password'];
-
-        //construct new user object
-        $f3->set('SESSION.user', new User());
-
-        //validate first name
-        if (validateName($firstName)) {
-            $f3->get('SESSION.user')->setFirstName($firstName);
-        } else {
-            $f3->set('errors["firstNameSignUpError"]', "please enter a valid first name");
-        }
-
-        //validate last name
-        if (validateName($lastName)) {
-            $f3->get('SESSION.user')->setLastName($lastName);
-        } else {
-            $f3->set('errors["lastNameSignUpError"]', "please enter a valid last name");
-        }
-
-        //validate UPS email
-        if (validateUPSEmail($email)) {
-            $f3->get('SESSION.user')->setEmail($email);
-        } else {
-            $f3->set('errors["emailSignUpError"]', "please enter a valid UPS email");
-        }
-
-        //validate slic
-        if (validSlic($slic)) {
-            $f3->get('SESSION.user')->setSlic($slic);
-        } else {
-            $f3->set('errors["slicSignUpError"]', "please enter a valid SLIC");
-        }
-
-        //role
-        if (validRole($role)) {
-            $f3->get('SESSION.user')->setRole($role);
-        } else {
-            $f3->set('errors["roleSignUpError"]', "please enter a valid role");
-        }
-
-        //password
-        if (validatePassword($password)) {
-            $f3->get('SESSION.user')->setPassword($password);
-        } else {
-            $f3->set('errors["passwordSignUpError"]', "please enter a valid password");
-        }
-
-        //check errors[]
-        if (empty($f3->get('errors'))) {
-            //send email to DM to approve sign up request and add to DB
-            $status = 0;
-            $date = date('Y-m-d h:i:s', time());
-            $subject = "Access Requested From: " . $f3->get('SESSION.user')->getFirstName()." ". $f3->get('SESSION.user')->getLastName();
-            $to = "garrett.ballreich101@gmail.com";
-            //msg sould be a link to appove the request
-            require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
-
-            try {
-                $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-                echo 'connected to database!';
-            } catch (PDOException $e) {
-                die($e->getMessage());
-            }
-
-            //First
-            $sql = 'INSERT INTO users (first_name, last_name, email, password, account_activated, role, created_at) 
-            VALUES(:First, :Last, :Email, :Password, :AccountActivated, :Role, :CreatedAt)';
-
-            $statement = $dbh->prepare($sql);
-            $statement->bindParam(':First', $firstName);
-            $statement->bindParam(':Last', $lastName);
-            $statement->bindParam(':Email', $email);
-            $statement->bindParam(':Password', $password);
-            $statement->bindParam(':AccountActivated', $status);
-            $statement->bindParam(':Role', $role);
-            $statement->bindParam(':CreatedAt', $date);
-            $statement->execute();
-
-            $msg = "https://garrettballreich.greenriverdev.com/328/business-leads/approval";
-            mail($to, $subject, $msg);
-
-            $f3->reroute("dashboard");
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/sign-up.html');
+/**
+ * Route to the signup page
+ */
+$f3-> route('GET|POST /sign-up', function() {
+    $GLOBALS['controller']->signUp();
 });
 
+/**
+ * Route to the approval page
+ */
 $f3-> route('GET|POST /approval', function() {
-    //Render a view page
-    //connect to db
-    require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
-
-    try {
-        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-        echo 'connected to database!';
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
-
-    $sql = "SELECT * FROM users WHERE account_activated = 0";
-
-    //prepare the statement
-    $statement = $dbh->prepare($sql);
-
-    //execute the statement
-    $statement->execute();
-
-    //process
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-    echo "<h1>Approve Requests</h1>";
-    echo "<ol>";
-    foreach($result as $row){
-        echo "<li>".$row['last_name']. ", ".$row['first_name']."-".$row['']."</li>";
-    }
-    echo "</ol>";
-    $view = new Template();
-    echo $view->render('views/approval.html');
+    $GLOBALS['controller']->approval();
 });
 
-//Define the MAIN FORM route
-$f3-> route('GET|POST /form', function($f3) {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $businessName = $_POST['businessName'];
-        $businessAddress = $_POST['businessAddress'];
-        $contactName = $_POST['firstName'];
-        $contactLastName = $_POST['lastName'];
-        $businessPhone = $_POST['phoneNum'];
-        $contactEmail = $_POST['email'];
-        $driverName = $_POST['driverName'];
-        $driverID = $_POST['driverID'];
-        $slic = $_POST['slic'];
-
-        //construct new lead object
-        //add it to the session
-        //with all unknown values until they pass validation
-        $f3->set("SESSION.lead", new Lead());
-        //set each variable to an object field
-
-        //validate name
-        //all letter and not null
-        if (validateName($businessName)) {
-            $f3->get('SESSION.lead')->setBusinessName($businessName);
-        } else {
-            $f3->set('errors["businessNameError"]', "please enter a valid name");
-        }
-
-        //validate contact name
-        if (validateName($contactName)) {
-            $f3->get('SESSION.lead')->setContactName($contactName);
-        } else {
-            $f3->set('errors["contactNameError"]', "please enter a valid contact name");
-        }
-
-        //validate contact lastname
-        if (validateName($contactLastName)) {
-            $f3->get('SESSION.lead')->setContactLastName($contactLastName);
-        } else {
-            $f3->set('errors["contactLastNameError"]', "please enter a valid contact last name");
-        }
-
-        //validate phone number
-        if (validPhone($businessPhone)) {
-            $f3->get('SESSION.lead')->setBusinessPhone($businessPhone);
-        } else {
-            $f3->set('errors["businessPhoneError"]', "please enter a valid phone number");
-        }
-
-        //validate email
-        if (validateEmail($contactEmail)) {
-            $f3->get('SESSION.lead')->setContactEmail($contactEmail);
-        } else {
-            $f3->set('errors["contactEmailError"]', "please enter a valid email address");
-        }
-
-        //validate driver name
-        if (validateName($driverName)) {
-            $f3->get('SESSION.lead')->setDriverName($driverName);
-        } else {
-            $f3->set('errors["driverNameError"]', "please enter a valid contact last name");
-        }
-
-        //validate employee ID
-        if (validEmployeeID($driverID)) {
-            $f3->get('SESSION.lead')->setDriverID($driverID);
-        } else {
-            $f3->set('errors["driverIDError"]', "please enter a valid employee ID");
-        }
-
-        //validate slic
-        if (validSlic($slic)) {
-            $f3->get('SESSION.lead')->setSlic($slic);
-        } else {
-            $f3->set('errors["slicError"]', "please enter a slic in the North West Division");
-        }
-
-        //check errors[]
-        if (empty($f3->get('errors'))) {
-            //add lead object to DB
-            //email lead object to perspective center manager
-            $f3->reroute('formSummary');
-        }
-    }
-
-    //Render a view page
-    $view = new Template();
-    echo $view->render('views/form.html');
+/**
+ * Route to the main form page
+ */
+$f3-> route('GET|POST /form', function() {
+    $GLOBALS['controller']->mainForm();
 });
 
 //Define the form summary route
-$f3-> route('GET /formSummary', function() {
-    //Render a view page
-    $view = new Template();
-    echo $view->render('views/formSummary.html');
+$f3-> route('GET /form-summary', function() {
+    $GLOBALS['controller']->formSummary();
 });
 
 //Define the dashboard route
 $f3-> route('GET /dashboard', function() {
-    //Render a view page
-    $view = new Template();
-    echo $view->render('views/dashboard.html');
+    $GLOBALS['controller']->dashboard();
 });
 
 //Run Fat-Free
